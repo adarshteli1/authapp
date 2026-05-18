1070087714641aa8ceb5f8db3c3921d64b20807e
Adarsh TEli <119785271+adarshteli1@users.noreply.github.com>
2026-05-18 04:04:43 -0400

    Refactor email verification logic and fix typos

--- src/app/api/Users/verifyemail/route.ts
+++ src/app/api/Users/verifyemail/route.ts
@@ -1,45 +1,39 @@
-import { connect } from "@/dbConfig/dbconfig";
-import User from "@/models/UserModel"
-import { error } from "console";
-import { verify } from "crypto";
-import { NextRequest,NextResponse } from "next/server";
-import { json } from "stream/consumers";
+import {connect} from "@/dbConfig/dbConfig";
+import { NextRequest, NextResponse } from "next/server";
+import User from "@/models/userModel";
+
+
 
 connect()
 
+
 export async function POST(request: NextRequest){
+
     try {
         const reqBody = await request.json()
         const {token} = reqBody
-        console.log(reqBody);
+        console.log(token);
 
-        const user = await User.findOne({verifyToken: token,
-            verifyTokenExpiry:{$gt: Date.now()}
-        })
+        const user = await User.findOne({verifyToken: token, verifyTokenExpiry: {$gt: Date.now()}});
 
-        if(!user){
-            return NextResponse.json({error : "Invalid token"},
-               {status:400})
+        if (!user) {
+            return NextResponse.json({error: "Invalid token"}, {status: 400})
         }
         console.log(user);
 
-        user.isVerified=true
-        user.verifyToken=undefined
-        user.verifyTokenExpiry= undefined
-
-        await user.save()
+        user.isVerfied = true;
+        user.verifyToken = undefined;
+        user.verifyTokenExpiry = undefined;
+        await user.save();
         
         return NextResponse.json({
-            message: "Email verified Sucessfully",
+            message: "Email verified successfully",
             success: true
-        },
-        {status:500})
+        })
 
-    } 
-    
-    catch (error) {
-        return NextResponse.json({error: (error as Error).message}),
-        {status:500}
+
+    } catch (error:any) {
+        return NextResponse.json({error: error.message}, {status: 500})
     }
 
 }

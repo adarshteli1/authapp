829adae94c54a3be614334b24a2da9153d6d953d
Adarsh TEli <119785271+adarshteli1@users.noreply.github.com>
2026-05-18 04:04:18 -0400

    Refactor signup route and improve error handling

--- src/app/api/Users/singnup/route.ts
+++ src/app/api/Users/singnup/route.ts
@@ -1,50 +1,55 @@
-import {connect} from '@/dbConfig/dbconfig'
-import User from '@/models/UserModel'
-import {NextRequest, NextResponse } from 'next/server'
-import bcryptjs from 'bcryptjs'
-import { sendMail } from '@/helpers/mailer'
+import {connect} from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+import { sendEmail } from "@/helpers/mailer";
+
 
 connect()
 
-export async function POST(request: NextRequest) {
+
+export async function POST(request: NextRequest){
     try {
-        const reqBody=await request.json()
-        const {username,email,password} = reqBody
+        const reqBody = await request.json()
+        const {username, email, password} = reqBody
 
         console.log(reqBody);
 
-        const user= await User.findOne({email})
-        if(user)
-        {
-            return NextResponse.json({error: "User ALready Exist" },
-            {status:400}
-            )
+        //check if user already exists
+        const user = await User.findOne({email})
+
+        if(user){
+            return NextResponse.json({error: "User already exists"}, {status: 400})
         }
 
-        const salt = await bcryptjs.genSalt(10);
-        const hashedPassword= await bcryptjs.hash(password,salt)
-        
+        //hash password
+        const salt = await bcryptjs.genSalt(10)
+        const hashedPassword = await bcryptjs.hash(password, salt)
+
         const newUser = new User({
             username,
             email,
             password: hashedPassword
         })
 
-        const savedUser=await newUser.save()
+        const savedUser = await newUser.save()
         console.log(savedUser);
-        
-        //send Verification mail
 
-        await sendMail({email,emailType:"VERIFY",userId:savedUser._id})
+        //send verification email
+
+        await sendEmail({email, emailType: "VERIFY", userId: savedUser._id})
 
         return NextResponse.json({
-            message: "User registered Successfully",
-            sucess:true,
+            message: "User created successfully",
+            success: true,
             savedUser
         })
+        
+        
 
-    } catch (error) {
-        return NextResponse.json({error: (error as Error).message}),
-        {status:500}
+
+    } catch (error: any) {
+        return NextResponse.json({error: error.message}, {status: 500})
+
     }
 }
